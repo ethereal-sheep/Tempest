48a06426cb4cf75812440a43f745089bf1aede1f
xLinus20 <linusng97@gmail.com>
2021-05-29 22:59:25 +0800

    Window Creation
    Error in building the editor obj file.
    Cant link to entry point

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -1,6 +1,6 @@
 
 #define _CRT_SECURE_NO_WARNINGS
-
+#include "Application/EntryPoint.h"
 #include "Util.h"
 #include "Instance/Instances/RuntimeInstance.h"
 #include "Events/Test/event_test.h"
@@ -11,17 +11,47 @@
 using namespace std;
 using namespace Tempest;
 
-
-
-int main()
+class Editor : public Application
 {
-	// always call this first
-	Logger::Init();
-	LOG("Initializing Tempest");
-	const char* s = R"(S:\Development\Projects)";
-	tpath path(s);
-	
-	RuntimeInstance i(path, MemoryStrategy{DebugFlag::NONE});
-	LOG("{}", i.has_debug());
+public:
 
+	Editor()
+		: Application(1080, 960, L"Editor") {}
+
+	void OnInit() override
+	{
+		Logger::Init();
+		LOG("Initializing Tempest");
+		const char* s = R"(S:\Development\Projects)";
+		tpath path(s);
+
+		RuntimeInstance i(path, MemoryStrategy{ DebugFlag::NONE });
+		LOG("{}", i.has_debug());
+	}
+
+	void OnUpdate() override
+	{
+
+	}
+
+	void OnRender() override
+	{
+
+	}
+
+	void OnExit() override
+	{
+
+	}
+};
+
+std::unique_ptr<Tempest::Application> CreateApplication()
+{
+	return std::make_unique<Editor>();
 }
+//int main()
+//{
+//	// always call this first
+//	
+//
+//}
