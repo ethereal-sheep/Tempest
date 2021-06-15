baccce19d3571aac3ef561ad3b6e334fd437860c
ethereal-sheep <55541825+ethereal-sheep@users.noreply.github.com>
2021-06-16 01:10:14 +0800

    push some style

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -21,6 +21,9 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 
 namespace Tempest
 {
+	void init_font();
+	void init_style();
+
 	class Editor : public Application
 	{
 
@@ -48,6 +51,7 @@ namespace Tempest
 			}
 
 			init_font();
+			init_style();
 
 		}
 
@@ -98,10 +102,11 @@ namespace Tempest
 
 					}
 					ImGui::PushFont(FONT_FAD);
-					if (ImGui::MenuItem(ICON_FAD_RANDOM_1DICE " Font Audio"))
+					if (ImGui::MenuItem(ICON_FAD_RANDOM_1DICE ICON_FAD_RANDOM_2DICE))
 					{
 
 					}
+
 					ImGui::PopFont();
 					ImGui::PushFont(FONT_FK);
 					if (ImGui::MenuItem(ICON_FK_MAP_PIN " Fork Awesome"))
@@ -129,7 +134,7 @@ namespace Tempest
 				if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 				{
 					ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-					ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_AutoHideTabBar);
+					ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), 0);
 				}
 			}
 
@@ -163,5 +168,128 @@ namespace Tempest
 	std::unique_ptr<Tempest::Application> CreateApplication()
 	{
 		return std::make_unique<Editor>();
+	}
+
+	void init_style()
+	{
+		auto& style = ImGui::GetStyle();
+
+		ImVec4* colors = style.Colors;
+		colors[ImGuiCol_Text] = ImVec4(0.90f, 0.90f, 0.90f, 1.00f);
+		colors[ImGuiCol_TextDisabled] = ImVec4(0.60f, 0.60f, 0.60f, 1.00f);
+		colors[ImGuiCol_WindowBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.85f);
+		colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		colors[ImGuiCol_PopupBg] = ImVec4(0.11f, 0.11f, 0.14f, 0.92f);
+		colors[ImGuiCol_Border] = ImVec4(0.50f, 0.50f, 0.50f, 0.50f);
+		colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		colors[ImGuiCol_FrameBg] = ImVec4(0.43f, 0.43f, 0.43f, 0.39f);
+		colors[ImGuiCol_FrameBgHovered] = ImVec4(0.47f, 0.47f, 0.69f, 0.40f);
+		colors[ImGuiCol_FrameBgActive] = ImVec4(0.42f, 0.41f, 0.64f, 0.69f);
+		colors[ImGuiCol_TitleBg] = ImVec4(0.27f, 0.27f, 0.54f, 0.83f);
+		colors[ImGuiCol_TitleBgActive] = ImVec4(0.32f, 0.32f, 0.63f, 0.87f);
+		colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.40f, 0.40f, 0.80f, 0.20f);
+		colors[ImGuiCol_MenuBarBg] = ImVec4(0.40f, 0.40f, 0.55f, 0.80f);
+		colors[ImGuiCol_ScrollbarBg] = ImVec4(0.20f, 0.25f, 0.30f, 0.60f);
+		colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.40f, 0.40f, 0.80f, 0.30f);
+		colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.40f, 0.40f, 0.80f, 0.40f);
+		colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.41f, 0.39f, 0.80f, 0.60f);
+		colors[ImGuiCol_CheckMark] = ImVec4(0.90f, 0.90f, 0.90f, 0.50f);
+		colors[ImGuiCol_SliderGrab] = ImVec4(1.00f, 1.00f, 1.00f, 0.30f);
+		colors[ImGuiCol_SliderGrabActive] = ImVec4(0.41f, 0.39f, 0.80f, 0.60f);
+		colors[ImGuiCol_Button] = ImVec4(0.35f, 0.40f, 0.61f, 0.62f);
+		colors[ImGuiCol_ButtonHovered] = ImVec4(0.40f, 0.48f, 0.71f, 0.79f);
+		colors[ImGuiCol_ButtonActive] = ImVec4(0.46f, 0.54f, 0.80f, 1.00f);
+		colors[ImGuiCol_Header] = ImVec4(0.40f, 0.40f, 0.90f, 0.45f);
+		colors[ImGuiCol_HeaderHovered] = ImVec4(0.45f, 0.45f, 0.90f, 0.80f);
+		colors[ImGuiCol_HeaderActive] = ImVec4(0.53f, 0.53f, 0.87f, 0.80f);
+		colors[ImGuiCol_Separator] = ImVec4(0.50f, 0.50f, 0.50f, 0.60f);
+		colors[ImGuiCol_SeparatorHovered] = ImVec4(0.60f, 0.60f, 0.70f, 1.00f);
+		colors[ImGuiCol_SeparatorActive] = ImVec4(0.70f, 0.70f, 0.90f, 1.00f);
+		colors[ImGuiCol_ResizeGrip] = ImVec4(1.00f, 1.00f, 1.00f, 0.10f);
+		colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.78f, 0.82f, 1.00f, 0.60f);
+		colors[ImGuiCol_ResizeGripActive] = ImVec4(0.78f, 0.82f, 1.00f, 0.90f);
+		colors[ImGuiCol_Tab] = ImVec4(0.34f, 0.34f, 0.68f, 0.79f);
+		colors[ImGuiCol_TabHovered] = ImVec4(0.45f, 0.45f, 0.90f, 0.80f);
+		colors[ImGuiCol_TabActive] = ImVec4(0.40f, 0.40f, 0.73f, 0.84f);
+		colors[ImGuiCol_TabUnfocused] = ImVec4(0.28f, 0.28f, 0.57f, 0.82f);
+		colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.35f, 0.35f, 0.65f, 0.84f);
+		colors[ImGuiCol_DockingPreview] = ImVec4(0.40f, 0.40f, 0.90f, 0.31f);
+		colors[ImGuiCol_DockingEmptyBg] = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+		colors[ImGuiCol_PlotLines] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+		colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+		colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+		colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+		colors[ImGuiCol_TableHeaderBg] = ImVec4(0.27f, 0.27f, 0.38f, 1.00f);
+		colors[ImGuiCol_TableBorderStrong] = ImVec4(0.31f, 0.31f, 0.45f, 1.00f);
+		colors[ImGuiCol_TableBorderLight] = ImVec4(0.26f, 0.26f, 0.28f, 1.00f);
+		colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+		colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.07f);
+		colors[ImGuiCol_TextSelectedBg] = ImVec4(0.00f, 0.00f, 1.00f, 0.35f);
+		colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+		colors[ImGuiCol_NavHighlight] = ImVec4(0.45f, 0.45f, 0.90f, 0.80f);
+		colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+		colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+		colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+
+
+		style.WindowRounding = 5;
+		style.ChildRounding = 5;
+		style.FrameRounding = 5;
+		style.PopupRounding = 5;
+		style.ScrollbarRounding = 5;
+		style.GrabRounding = 5;
+		style.TabRounding = 5;
+
+		style.WindowTitleAlign = { 0.5f, 0.5f };
+		style.WindowMenuButtonPosition = ImGuiDir_Right;
+		
+		style.ButtonTextAlign = { 0.5f, 0.5f };
+		style.SelectableTextAlign = { 0.0f, 0.5f };
+
+		style.WindowBorderSize = 0;
+		style.FrameBorderSize = 0;
+		style.PopupBorderSize = 0;
+	}
+
+	void init_font()
+	{
+		auto io = ImGui::GetIO();
+		ImFontConfig config;
+		config.MergeMode = true;
+		config.GlyphMinAdvanceX = 13.0f; // Use if you want to make the icon monospaced
+		config.PixelSnapH = true;
+
+		tpath def_f = tpath("Fonts") / default_font;
+		tpath bold_f = tpath("Fonts") / bold_font;
+		def_f.replace_extension(".ttf");
+		bold_f.replace_extension(".ttf");
+
+
+		static const ImWchar fa_range[] = { ICON_MIN_FA, ICON_MAX_FA, 0 };
+		io.Fonts->AddFontFromFileTTF(def_f.string().c_str(), font_text_size); // change this to change default font
+		io.Fonts->AddFontFromFileTTF("Fonts/fa-regular-400.ttf", font_icon_size, &config, fa_range);
+		io.Fonts->AddFontFromFileTTF("Fonts/fa-solid-900.ttf", font_icon_size, &config, fa_range);
+
+		io.Fonts->AddFontFromFileTTF(bold_f.string().c_str(), font_text_size);
+
+		static const ImWchar fk_range[] = { ICON_MIN_FK, ICON_MAX_FK, 0 };
+		io.Fonts->AddFontFromFileTTF(def_f.string().c_str(), font_text_size);
+		io.Fonts->AddFontFromFileTTF("Fonts/forkawesome-webfont.ttf", font_icon_size, &config, fk_range);
+
+		config.PixelSnapH = true;
+		config.GlyphOffset = { 0,2 };
+		static const ImWchar fad_range[] = { ICON_MIN_FAD, ICON_MAX_FAD, 0 };
+		io.Fonts->AddFontFromFileTTF(def_f.string().c_str(), font_text_size);
+		io.Fonts->AddFontFromFileTTF("Fonts/fontaudio.ttf", font_icon_size, &config, fad_range);
+
+		static const ImWchar ki_range[] = { ICON_MIN_KI, ICON_MAX_KI, 0 };
+		io.Fonts->AddFontFromFileTTF(def_f.string().c_str(), font_text_size);
+		io.Fonts->AddFontFromFileTTF("Fonts/kenney-icon-font.ttf", font_icon_size, &config, ki_range);
+
+		def = io.Fonts->Fonts[0];
+		bold = io.Fonts->Fonts[1];
+		fk = io.Fonts->Fonts[2];
+		fad = io.Fonts->Fonts[3];
+		ki = io.Fonts->Fonts[4];
 	}
 }
