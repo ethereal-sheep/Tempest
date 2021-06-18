07a73292657764ba14b1161c6a304673b54b03c7
ethereal-sheep <55541825+ethereal-sheep@users.noreply.github.com>
2021-06-18 18:04:51 +0800

    added some comments for physics

--- Tempest/src/Physics/PhysicsObject.h
+++ Tempest/src/Physics/PhysicsObject.h
@@ -14,6 +14,38 @@ namespace Tempest
 		// bunch of other shit
 	};
 
+	/* three main type for collision i think
+	* 
+	* 1. trigger (no collision at all, only triggers events)
+	*		actually might not need this since everything is grid based in software
+	*		and for 300 we are just doing random demos
+	*		lmk what yall think
+	* 2. static 
+	*		doesn't move, collides with all dynamic types
+	* 3. dynamic
+	*		is simulated by physx, collides with everything
+	* 4. particles (dk if this is here anot) // 
+			might be some special type
+	*		that we define outside
+	*		
+	*			
+	*/
+	struct rigidbody_config
+	{
+		float mass;
+		float density;
+		bool static_or_dynamic; // can make enum if you envision future types
+		bool gravity; // explicit or implicit with the static/dynamic type
+
+		// just reference for you
+		/*
+		* PxMaterial needs this three things to be created
+			PxReal staticFriction, PxReal dynamicFriction, PxReal restitution
+		*/
+	};
+
+	
+
 	struct sample_rigidbody
 	{
 		// maybe? implementation
@@ -22,6 +54,48 @@ namespace Tempest
 		 * @brief MUST BE CREATED WITH px_make!
 		 * We can possibly make it only initializable with PhysicsObject.
 		 */
+
+		 // gravity yes:no
+		 // masss
+		 // density 
+		 // material -> default/preset donnid to be adjusted by designer
+		 // resolve collision yes:no () 
+
+		 // shape (capsule, sphere, box, )
+		// mentioned earlier, complex shapes need cooking and we said we don't need
+		// however, if we want to simulate dice roll (we need complex shapes)
+		// so just do all the standard dice first ()
+		// d4 (tetrahedron, tetrapyramid, 4 triangles glued-tgt)
+		// d6 (cube)
+		// d8 (octahedron (2 tetrahedron))
+		// d10 (no fking idea what shape is this but is
+		// d12 (dodecahedron)
+		// d20 icosahedron
+		/*
+		* 
+		*	[0, 2, 3],
+			[0, 3, 4],
+			[0, 4, 5],
+			[0, 5, 6],
+			[0, 6, 7],
+			[0, 7, 8],
+			[0, 8, 9],
+			[0, 9, 10],
+			[0, 10, 11],
+			[0, 11, 2],
+			[1, 3, 2],
+			[1, 4, 3],
+			[1, 5, 4],
+			[1, 6, 5],
+			[1, 7, 6],
+			[1, 8, 7],
+			[1, 9, 8],
+			[1, 10, 9],
+			[1, 11, 10],
+			[1, 2, 11]
+		
+		*/
+		 // default 
 		tsptr<physx::PxActor> actor;
 
 	};
@@ -30,6 +104,7 @@ namespace Tempest
 	class PhysicsObject
 	{
 	public:
+		// create plane in constructor
 		PhysicsObject(m_resource* mem_res = aligned_malloc_resource());
 
 		// testing
@@ -44,6 +119,13 @@ namespace Tempest
 		 * #notes
 		 * PxTriangleMesh, PxHeightField, PxConvexMesh, PxMaterial, and PxShape can auto deleted
 		 * so no need px_make, unless no references (actually not sure need db check)
+		 *  
+		 * 0. do we want dedicate system to handle shapes? reusing shapes are good way of
+		 *	saving memory but because we cannot change the size of a shape after creation time
+		 *  this may be a bad idea. this is the same for materials.
+		 *		since we are using grids for many software, we can configure shapes to be exactly
+		 *		1x1. But this defeats the purpose of having physics simulation in the first place.
+		 *      
 		 *
 		 * 1. add actors to the scene?
 		 *		can we use shared_ptr?
@@ -65,7 +147,7 @@ namespace Tempest
 		 *		using share pointer means we can easily remove stuff
 		 *		just call scene->removeActor(shared_p.get())
 		 *			den we just let the shared_ptr destructor take care of the rest
-		 *		or we can readd it to the scene later if we want, doesnt matter
+		 *		or we can re-add it to the scene later if we want, doesnt matter
 		 *			cos it only dies after sharePtr goes out of scope
 		 *
 		 * 3. physics use cases
@@ -79,7 +161,18 @@ namespace Tempest
 		 *
 		 */
 
-		
+		tsptr<physx::PxRigidBody> createActor(vec3 pos, quat rotation, rigidbody_config config) // some other stuff for configuration)
+		{
+			float h_extents = 0.5f;
+			auto material = physics->createMaterial(0.5f, 0.5f, 0.6f);
+			PxBoxGeometry a(h_extents, h_extents, h_extents);
+			auto shape = physics->createShape(a, *material);
+		}
+
+		tvector<tsptr<physx::PxRigidBody>> createParticles() // some stuff for configuration)
+		{
+
+		}
 
 	private:
 		px_allocator allocator;
