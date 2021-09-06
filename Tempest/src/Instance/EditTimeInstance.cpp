89a91ee38ff53adf31df43497f0ac83b12ebae00
Huang Xu Rong <h_rongrong@hotmail.com>
2021-09-06 16:05:16 +0800

    Updates

--- Tempest/src/Instance/EditTimeInstance.cpp
+++ Tempest/src/Instance/EditTimeInstance.cpp
@@ -9,11 +9,16 @@ namespace Tempest
 	{
 		auto view = ecs.view<Components::Rigidbody, tc::Transform>(exclude_t<tc::Destroyed>());
 
+
+		auto sl = ecs.view<Components::Statline>(exclude_t<tc::Destroyed>());
+		if (sl.size_hint() == 0)
+		{
+			auto StatsLine = ecs.create();
+			auto meta = ecs.emplace<tc::Meta>(StatsLine);
+			meta->name = "StatsData";
+			ecs.emplace<tc::Statline>(StatsLine);
+		}
 		
-		//auto StatsLine = ecs.create();
-		//auto meta = ecs.emplace<tc::Meta>(StatsLine);
-		//meta->name = "StatsData";
-		//ecs.emplace<tc::Statline>(StatsLine);
 
 		for (auto id : view)
 		{
