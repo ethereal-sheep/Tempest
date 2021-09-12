b20fd33315cf575b249f9e49d3fc78f298bb2669
ethereal-sheep <55541825+ethereal-sheep@users.noreply.github.com>
2021-09-13 00:38:05 +0800

    stats stuff seems like it worls

--- Tempest/src/Scripting/Nodes/StatNode.cpp
+++ Tempest/src/Scripting/Nodes/StatNode.cpp
@@ -7,16 +7,32 @@ namespace Tempest
     std::string GetStatNode::get_type_string()
     {
         // assuming no problem
-        std::string toReturn = get_name();
+        std::string toReturn = magic_enum::enum_name(type).data();
+        toReturn += ":";
+        toReturn += get_name();
 
         return std::move(toReturn);
     }
 
     node_ptr GetStatNode::create_node(const std::string& typeinfo)
     {
+        // parse type info
+        auto v = algo::delimited_split(typeinfo, ":");
+        if (v.size() != 2)
+        {
+            throw node_exception("GetStatNode: Bad inner type!");
+        }
+        // attacker/defender | stat_id
+        auto type = magic_enum::enum_cast<GetStatNode::inner_type>(v[0]);
+
+        if (!type.has_value())
+        {
+            throw node_exception("GetStatNode: Bad inner type!");
+        }
+
         // we cannot check if stat exist here so we assume it does
-        GetStatNode node(GetStatNode::inner_type::_cannot_be_empty);
-        node.set_name(typeinfo);
+        GetStatNode node(type.value());
+        node.set_name(v[1]);
         node.add_output(pin_type::Int, "");
 
         return make_sptr<GetStatNode>(std::move(node));
@@ -36,30 +52,63 @@ namespace Tempest
             statline = instance.ecs.get_if<tc::Statline>(i);
 
         // no statline or no stat, throw
-        if (!statline || !statline->exist(get_name())) return nullptr; // might want reasonable msg here
+        if (!statline) throw script_exception("Bad Script! No Statline exists!");
 
         // get index of stat in statline
-        auto index = statline->index_of_stat(get_name());
+        auto stat_id = std::stoi(get_name());
+        if (stat_id >= statline->size()) throw script_exception("Bad Script! No Stat exists!");
         
-        // if have character sheet
+        switch (type)
+        {
+        case Tempest::GetStatNode::inner_type::Attacker:
             return instance.srm.add_script(
                 CreateConstexprScript<std::tuple<int>(void)>(
                     [&]() {
 
-                        if (auto character = instance.ecs.get_if<tc::Character>(entity))
+                        auto atker = instance.srm.get_variable_to_id(entity, "Attacker"); // attacking entity
+
+                        Entity atk_e = (Entity)atker->get<int64_t>();
+
+                        if (auto character = instance.ecs.get_if<tc::Character>(atk_e))
                         {
-                            auto& stat = character->get_stat(index);
+                            auto& stat = character->get_stat(stat_id);
 
                             if (auto weapon = instance.ecs.get_if<tc::Weapon>(character->weapon))
                             {
-                                return stat + weapon->get_stat(index);
+                                return stat + weapon->get_stat(stat_id);
                             }
                             return stat;
                         }
                         return 0;
                     }));
-        
+            break;
+        case Tempest::GetStatNode::inner_type::Defender:
+            return instance.srm.add_script(
+                CreateConstexprScript<std::tuple<int>(void)>(
+                    [&]() {
+                        auto defer = instance.srm.get_variable_to_id(entity, "Defender"); // defending entity
 
+                        Entity def_e = (Entity)defer->get<int64_t>();
+
+                        if (auto character = instance.ecs.get_if<tc::Character>(def_e))
+                        {
+                            auto& stat = character->get_stat(id);
+
+                            if (auto weapon = instance.ecs.get_if<tc::Weapon>(character->weapon))
+                            {
+                                return stat + weapon->get_stat(id);
+                            }
+                            return stat;
+                        }
+                        return 0;
+                    }));
+            break;
+        default:
+            break;
+        }
+
+        return nullptr;
+            
     }
 
     std::string SetStatNode::get_type_string()
