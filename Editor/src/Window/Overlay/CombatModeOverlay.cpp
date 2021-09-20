fb21a8d4816aa96fd08e4c2c06093085de6a3220
xLinus20 <linusng97@gmail.com>
2021-09-20 15:13:54 +0800

    commented out elmo

--- Editor/src/Window/Overlay/CombatModeOverlay.cpp
+++ Editor/src/Window/Overlay/CombatModeOverlay.cpp
@@ -1,6 +1,6 @@
 #include "CombatModeOverlay.h"
 #include "Tempest/src/Graphics/OpenGL/Texture.h"
-#include "Tempest/src/Graphics/OpenGL/RenderSystem.h"
+#include "Tempest/src/Graphics/Basics/RenderSystem.h"
 
 namespace Tempest
 {
@@ -29,30 +29,30 @@ namespace Tempest
 
                 
                 auto drawlist = ImGui::GetWindowDrawList();
-                auto jankImg = Service<RenderSystem>::Get().jank2->GetID();
-				GLuint tex_id = static_cast<GLuint>(jankImg);
+                //auto jankImg = Service<RenderSystem>::Get().jank2->GetID();
+				//GLuint tex_id = static_cast<GLuint>(jankImg);
                 
 				ImGui::SetCursorPosY(ImGui::GetCursorPosY() + viewport->Size.y * 0.95f);
 				ImVec2 point = ImGui::GetCursorScreenPos();
                 // Setting character info bg
-                drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x - viewport->Size.x * 0.2f, point.y - viewport->Size.y * 0.2f), ImVec2(point.x + viewport->Size.x * 0.4f, point.y ));
+                //drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x - viewport->Size.x * 0.2f, point.y - viewport->Size.y * 0.2f), ImVec2(point.x + viewport->Size.x * 0.4f, point.y ));
                 //ImGui::Image((void*)static_cast<size_t>(tex_id), ImVec2(viewport->Size.x * 0.25f, viewport->Size.y * 0.25f));
 
 				// Setting bootiful elmo
-                jankImg = Service<RenderSystem>::Get().tex->GetID();
-				tex_id = static_cast<GLuint>(jankImg);
+                //jankImg = Service<RenderSystem>::Get().tex->GetID();
+				//tex_id = static_cast<GLuint>(jankImg);
 				
-				drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x, point.y - viewport->Size.y * 0.4f), ImVec2(point.x + viewport->Size.x * 0.2f, point.y));
+				//drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x, point.y - viewport->Size.y * 0.4f), ImVec2(point.x + viewport->Size.x * 0.2f, point.y));
 
 				// Shift position
 				ImGui::SetCursorPosX(ImGui::GetCursorPosX() + viewport->Size.x * 0.65f);
 				point = ImGui::GetCursorPos();
 
 				// Setting actions info bg
-				jankImg = Service<RenderSystem>::Get().jank1->GetID();
-				tex_id = static_cast<GLuint>(jankImg);
+				//jankImg = Service<RenderSystem>::Get().jank1->GetID();
+				//tex_id = static_cast<GLuint>(jankImg);
 
-				drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x, point.y - viewport->Size.y * 0.2f), ImVec2(point.x + viewport->Size.x * 0.4f, point.y));
+				//drawlist->AddImage((void*)static_cast<size_t>(tex_id), ImVec2(point.x, point.y - viewport->Size.y * 0.2f), ImVec2(point.x + viewport->Size.x * 0.4f, point.y));
 			}
 
             ImGui::End();
