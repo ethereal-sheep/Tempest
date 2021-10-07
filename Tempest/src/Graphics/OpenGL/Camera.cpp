f6c5045cda8eaac89e6987cfbce078564a739a1f
Cantius Chew <55541825+ethereal-sheep@users.noreply.github.com>
2021-10-07 12:51:44 +0800

    tempest added header comment for all files

--- Tempest/src/Graphics/OpenGL/Camera.cpp
+++ Tempest/src/Graphics/OpenGL/Camera.cpp
@@ -1,3 +1,13 @@
+/**********************************************************************************
+* \author		_ (_@digipen.edu)
+* \version		1.0
+* \date			2021
+* \note			Course: GAM300
+* \copyright	Copyright (c) 2020 DigiPen Institute of Technology. Reproduction
+				or disclosure of this file or its contents without the prior
+				written consent of DigiPen Institute of Technology is prohibited.
+**********************************************************************************/
+
 #include "Camera.h"
 
 namespace Tempest
