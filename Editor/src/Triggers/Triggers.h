436b7758057521f097282b13dcc5f9cbc5bde2c7
Xurong <59607267+huangxurong@users.noreply.github.com>
2021-11-06 18:51:42 +0800

    comit

--- Editor/src/Triggers/Triggers.h
+++ Editor/src/Triggers/Triggers.h
@@ -84,20 +84,7 @@ namespace Tempest
 		bool for_unitpage;
 		Entity data;
 	};
-	struct AddingUnitsTrigger : public Event {};
-	struct AddingWeaponsTrigger : public Event {};
-	struct AddingActionsTrigger : public Event {};
-	struct SelectSequenceTrigger : public Event {};
-	struct CharacterStatsTrigger : public Event
-	{
-		CharacterStatsTrigger(Entity a) : entity{ a } {}
-		Entity entity;
-	};
-	struct WeaponStatsTrigger : public Event 
-	{
-		WeaponStatsTrigger(Entity a) : entity{ a } {}
-		Entity entity;
-	};
+
 
 	//Overlay Trigger
 	struct OpenSimulateTrigger : public Event {};
