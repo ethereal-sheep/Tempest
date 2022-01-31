aca65ca12abc4fdecec231b4b46de361e99083f4
geniejeans <lzyjeann@gmail.com>
2022-01-31 09:57:30 +0800

    unit sheet progress bar

--- Editor/src/Window/Overlay/UnitSheetOverlay.cpp
+++ Editor/src/Window/Overlay/UnitSheetOverlay.cpp
@@ -429,7 +429,7 @@ namespace Tempest
 						if (ImGui::IsMouseClicked(0))
 							instance.tutorial_enable = false;
 					}
-
+					UI::TutProgressBar(drawlist, ImVec2{ viewport->Size }, 1);
 				}
 
 			}
