89828e0413edf2b154dfe0fe29946bc3f1984058
ethereal-sheep <55541825+ethereal-sheep@users.noreply.github.com>
2022-02-08 22:12:59 +0800

    settings serialization

--- Editor/src/Window/Popup/SettingsPopup.h
+++ Editor/src/Window/Popup/SettingsPopup.h
@@ -16,6 +16,7 @@
 #include "Triggers/Triggers.h"
 #include "Audio/AudioEngine.h"
 
+
 namespace Tempest
 {
     class SettingsPopup : public Window
@@ -24,8 +25,16 @@ namespace Tempest
         {
             return "";
         }
+
+        void open_popup(const Event& e)
+        {
+            //auto a = event_cast<SettingsTrigger>(e);
+            enable_popup = true;
+        }
+
         void init(Instance&) override
         {
+
             window_flags =
                 ImGuiWindowFlags_NoDecoration |
                 ImGuiWindowFlags_NoDocking |
@@ -35,12 +44,98 @@ namespace Tempest
                 ImGuiWindowFlags_NoNav;
 
             Service<EventManager>::Get().register_listener<SettingsTrigger>(&SettingsPopup::open_popup, this);
+
+
+
+            AudioEngine ae;
+            // check APPDATA if there exist a file with project info
+            char* pValue;
+            size_t len;
+            [[maybe_unused]] errno_t err = _dupenv_s(&pValue, &len, "USERPROFILE");
+            if (!pValue)
+            {
+                LOG_WARN("APPDATA environment variable could not be found!");
+                return;
+            }
+            tpath path{ pValue };
+            free(pValue); // It's OK to call free with NULL
+            path /= "Documents";
+            path /= "CoReSys";
+            path /= "_settings.json";
+
+            Serializer serializer;
+            string json = serializer.GetJson(path);
+
+            Reader reader(json.c_str());
+            if (!reader.HasError())
+            {
+                reader.StartObject();
+                {
+                    float vol;
+                    reader.Member("Master", vol);
+                    ae.SetMasterVolume(std::clamp(vol, 0.f, 1.f));
+                }
+                {
+                    float vol;
+                    reader.Member("BGM", vol);
+                    ae.SetBusVolume("BGM", std::clamp(vol, 0.f, 1.f));
+                }
+                {
+                    float vol;
+                    reader.Member("SFX", vol);
+                    ae.SetBusVolume("SFX", std::clamp(vol, 0.f, 1.f));
+                }
+                {
+                    auto& gv = Service<RenderSystem>::Get().gammaValue;
+                    reader.Member("Gamma", gv);
+                }
+                reader.EndObject();
+            }
+            else
+                LOG("No existing settings found.");
         }
 
-        void open_popup(const Event& e)
+        void exit(Instance&) override
         {
-            //auto a = event_cast<SettingsTrigger>(e);
-            enable_popup = true;
+            AudioEngine ae;
+            // check APPDATA if there exist a file with project info
+            char* pValue;
+            size_t len;
+            [[maybe_unused]] errno_t err = _dupenv_s(&pValue, &len, "USERPROFILE");
+            if (!pValue)
+            {
+                LOG_WARN("APPDATA environment variable could not be found!");
+                return;
+            }
+            tpath envpath{ pValue };
+            free(pValue); // It's OK to call free with NULL
+            envpath /= "Documents";
+            envpath /= "CoReSys";
+
+            if (!fs::exists(envpath))
+                fs::create_directory(envpath);
+
+
+            Writer writer;
+            writer.StartObject();
+            {
+                float vol = ae.GetMasterVolume();
+                writer.Member("Master", vol);
+            }
+            {
+                float vol = ae.GetBusVolume("BGM");
+                writer.Member("BGM", vol);
+            }
+            {
+                float vol = ae.GetBusVolume("SFX");
+                writer.Member("SFX", vol);
+            }
+            {
+                auto& gv = Service<RenderSystem>::Get().gammaValue;
+                writer.Member("Gamma", gv);
+            }
+            writer.EndObject();
+            Serializer::SaveJson(envpath / "_settings.json", writer.GetString());
         }
 
         void show(Instance&) override
@@ -56,6 +151,7 @@ namespace Tempest
 
             ImVec2 center = ImGui::GetMainViewport()->GetCenter();
             ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+            ImGui::SetNextWindowSize(ImVec2(600.f, 400.f), ImGuiCond_Appearing);
             if (ImGui::BeginPopupModal(name, NULL, ImGuiWindowFlags_AlwaysAutoResize))
             {
                 if (ImGui::BeginTabBar("SettingsTabBar", ImGuiTabBarFlags_None))
