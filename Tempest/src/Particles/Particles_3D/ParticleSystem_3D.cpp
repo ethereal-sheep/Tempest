5ee8f02bcca6a19b5d5bd6cdc7b2cb1ef89dcf8a
PesudoSquidy <44610564+CJunHao@users.noreply.github.com>
2022-02-26 22:09:09 +0800

    Testing purpose

--- Tempest/src/Particles/Particles_3D/ParticleSystem_3D.cpp
+++ Tempest/src/Particles/Particles_3D/ParticleSystem_3D.cpp
@@ -61,3 +61,39 @@ void ParticleSystem_3D::AddEmitter(const std::shared_ptr<Emitter_3D> emitter)
 	else
 		m_Emitters.push_back(emitter);
 }
+
+
+const std::weak_ptr<Emitter_3D> ParticleSystem_3D::CreateTestEmitter(glm::vec3 spawnPos)
+{
+	auto tempEmitter = std::make_shared<Emitter_3D>();
+	Emitter_3D& explosionEmitter = *tempEmitter.get();
+	AddEmitter(tempEmitter);
+
+	// Emitter_2D values - Without consideration for default ctor values
+	explosionEmitter.m_GM.m_position = spawnPos;
+	//explosionEmitter.m_GM.m_velocity.x = -500.0f;
+	explosionEmitter.m_MM.m_duration = 2.f;
+	explosionEmitter.m_GM.m_active = true;
+	explosionEmitter.m_MM.m_preWarm = true;
+
+	explosionEmitter.m_EM.m_spawnTimeInterval = 2.f; // 5x slower of dt
+	explosionEmitter.m_EM.m_spawnCountTimer = explosionEmitter.m_EM.m_spawnTimeInterval;
+	explosionEmitter.m_EM.m_rateOverTime = 20;
+	explosionEmitter.m_MM.m_maxParticles = 1000;
+
+	// Particle Architype values - without consideration for default ctor
+	explosionEmitter.m_PAM.m_startVelocity = glm::vec3{ 0.f, 0.f, 0.0f };
+	explosionEmitter.m_PAM.m_endVelocity = glm::vec3{ 0.f, 0.f, 0.0f };
+	explosionEmitter.m_PAM.m_velocityVariation = glm::vec3{ 3.0f, 1.0f, 3.0f};
+
+	explosionEmitter.m_PAM.m_sizeBegin = 10.0f;
+	explosionEmitter.m_PAM.m_sizeEnd = 0.0f;
+	explosionEmitter.m_PAM.m_sizeVariation = 0.3f;
+
+	explosionEmitter.m_PAM.m_colourBegin = glm::vec4{ 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
+	explosionEmitter.m_PAM.m_colourEnd = glm::vec4{ 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 0.0f };
+
+	explosionEmitter.m_PAM.m_lifeTime = 0.3f;
+
+	return tempEmitter;
+}
--- Tempest/src/Particles/Particles_3D/ParticleSystem_3D.h
+++ Tempest/src/Particles/Particles_3D/ParticleSystem_3D.h
@@ -7,12 +7,16 @@
 #include "Core.h"
 #include "Emitter_3D.h"
 
+#include <glm.hpp>
+
 struct ParticleSystem_3D
 {
 	static ParticleSystem_3D& GetInstance();
 
 	void Update();
 	auto GetEmitter() const;
+
+	const std::weak_ptr<Emitter_3D> CreateTestEmitter(glm::vec3 spawnPos);
 
 private:
 	ParticleSystem_3D();
