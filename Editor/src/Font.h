b9a0ce51fdba4f239165c084baca286956bfb66c
Xurong <59607267+huangxurong@users.noreply.github.com>
2022-04-12 22:04:39 +0800

    Overlay folder

--- Editor/src/Font.h
+++ Editor/src/Font.h
@@ -4,7 +4,7 @@
 *				Huang Xurong(h.xurong@digipen.edu)
 * \version		1.0
 * \date			2021
-* \note			Course: GAM300
+* \note			Course: GAM350
 * \copyright	Copyright (c) 2020 DigiPen Institute of Technology. Reproduction
 				or disclosure of this file or its contents without the prior
 				written consent of DigiPen Institute of Technology is prohibited.
--- Editor/src/Window/Overlay/UnitSheetOverlay.cpp
+++ Editor/src/Window/Overlay/UnitSheetOverlay.cpp
@@ -1,9 +1,9 @@
 /**********************************************************************************
-* \author		Lim Ziyi Jean(ziyijean.lim@digipen.edu),
-*				Huang Xurong(h.xurong@digipen.edu)
+* \author		Huang Xurong(h.xurong@digipen.edu),
+				Lim Ziyi Jean(ziyijean.lim@digipen.edu)
 * \version		1.0
-* \date			2021
-* \note			Course: GAM300
+* \date			2022
+* \note			Course: GAM350
 * \copyright	Copyright (c) 2020 DigiPen Institute of Technology. Reproduction
 				or disclosure of this file or its contents without the prior
 				written consent of DigiPen Institute of Technology is prohibited.
